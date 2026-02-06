ec477fd706bd3c1918bf7fa0b0245babb709f89c
deidexdd-hash <deidex.dd@gmail.com>
2026-02-06 21:15:46 +0400

    Delete config.py

--- config.py
+++
@@ -1,9 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-class Config:
-    BOT_TOKEN      = os.getenv("BOT_TOKEN")
-    GROQ_API_KEY   = os.getenv("GROQ_API_KEY")
-    GROQ_MODEL     = os.getenv("GROQ_MODEL", "mixtral-8x7b-32768")
